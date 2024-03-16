894ff9cda968bf0dedc0339220e5bdaef6876ac4
ritesh1712 <105135021+ritesh1712@users.noreply.github.com>
2024-03-16 12:34:00 +0530

    change in app.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 
 function App() {
 
-  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         ritesh raghuwanshi
         </p>
         <a
           className="App-link"
